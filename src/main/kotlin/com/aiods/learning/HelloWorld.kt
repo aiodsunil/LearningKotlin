888a287a8813655cc42ba833285e27eaa8a574e8
Sunil Yadav <sunilyadav@Sunils-MacBook-Pro.local>
2021-01-20 23:11:54 +0000

    added comments

--- src/main/kotlin/com/aiods/learning/HelloWorld.kt
+++ src/main/kotlin/com/aiods/learning/HelloWorld.kt
@@ -1,11 +1,20 @@
 package com.aiods.learning
 
+/**
+ * this is multiline comment
+ */
+
 fun main() {
     print("Hello World!")
-    println("Hello Again")
+    //this is the hello world
+    println("Hello Again")//this is printing hello again
     var z= sum(5,6)
-    print("com.aiods.learning.sum of x and y is $z")
+    print("sum of x and y is $z")
+    
 }
 
+/**
+ * this is the sum which taking two number and returning sum it
+ */
 fun sum(x: Int, y: Int)=x+y
 
