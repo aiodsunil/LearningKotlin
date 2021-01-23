66ad73f23b6b4ca7b214e10c78aed999e4444a22
Sunil Yadav <aiodsunil@gmail.com>
2021-01-23 11:54:50 +0000

    double data types

--- src/main/kotlin/com/aiods/learning/DataType.kt
+++ src/main/kotlin/com/aiods/learning/DataType.kt
@@ -1,2 +1,16 @@
 package com.aiods.learning
 
+fun main() {
+    val  doubleType=33.75
+
+    println("doubleType is ${doubleType::class.qualifiedName}")
+    println("doubleType in java ${doubleType.javaClass}")
+
+    val intTye=123
+
+    println("intTye is ${intTye::class.qualifiedName}")
+    println("intTye in java ${intTye.javaClass}")
+
+    val longType=3344L
+    val floatType=343.54f
+}
