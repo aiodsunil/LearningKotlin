3de007242f13e455f3b70474c352652a57239073
Sunil Yadav <aiodsunil@gmail.com>
2021-01-23 16:52:49 +0000

    double data types

---
+++ src/main/kotlin/com/aiods/learning/OtherDataTypes.kt
@@ -0,0 +1,15 @@
+package com.aiods.learning
+
+import java.math.BigDecimal
+
+fun main() {
+
+  val bigDecimal=  BigDecimal(90)
+    println(bigDecimal)
+    val bigDecimalOther = bigDecimal.add(BigDecimal(10))
+  println(bigDecimalOther)
+    val doubleTupe:Double=123.89
+
+
+
+}
