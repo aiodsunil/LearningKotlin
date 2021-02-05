ce334cae2670917bca9367136ca4fcfc7026b3ae
Sunil Yadav <aiodsunil@gmail.com>
2021-02-05 23:06:30 +0000

    if keyword as expression

--- src/main/kotlin/com/aiods/learning/classes/Classes.kt
+++ src/main/kotlin/com/aiods/learning/classes/Classes.kt
@@ -2,23 +2,26 @@ package com.aiods.learning.classes
 
 data class Employee(val name: String,val age: Int,var address:String=""){
 
-    var surName:String?=null
+    var letter:String?=null
 
-    fun toUpperCase(){
-        surName?.toUpperCase() ?: ""
+    fun lastWord(s:String)=s.takeLast(1)
+
+    fun convert():String{
+        return letter?.let { lastWord(it) } ?: "test"
     }
+
 
 }
 
 fun main() {
 
     val emp1=Employee("max",33)
-    val emp2=Employee("mat",34)
-    val emp= if (emp1.age>emp2.age) emp1 else  emp2
-//    val emp= compare(emp1,emp2)
 
-    println("${emp.name},${emp.age}")
+     println(emp1.convert())
+    emp1.letter="Hello World"
+    println(emp1.convert())
+
+
 }
 
-//fun compare(emp1: Employee, emp2: Employee) = if (emp1.age>emp2.age) emp1 else  emp2
 
